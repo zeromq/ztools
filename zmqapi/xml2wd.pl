aec0bde7c00c58450e95baa5994b4feb7807f03d
Kevin Sapper <mail@kevinsapper.de>
2017-02-15 09:11:30 +0100

    Problem: zmq API points to the wrong pages
    Solution: Fix the page and make the build faster

--- zmqapi/xml2wd.pl
+++ zmqapi/xml2wd.pl
@@ -46,7 +46,7 @@ END
         if ($title eq "zmq") {
             open (TOC, ">_start.wd");
             print TOC "++ ØMQ/$version API Reference\n\n";
-            print TOC "[/ZMQ-master:_start v4.2 master] | [/ZMQ4-2:_start v4.2 stable] | [/ZMQ4-1:_start v4.1 stable] | [/ZMQ4-0:_start v4.0 stable] | [/ZMQ3-2:_start v3.2 legacy]\n\n";
+            print TOC "[/master:_start v4.2 master] | [/4-2:_start v4.2 stable] | [/4-1:_start v4.1 stable] | [/4-0:_start v4.0 stable] | [/3-2:_start v3.2 legacy]\n\n";
             close (TOC);
         }
     }
